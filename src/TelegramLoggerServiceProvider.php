62c78dcda61dff3d587bd2b44b274b8988f5d70b
Nuno Franca <contato@nunotech.com.br>
2022-08-10 10:37:19 -0300

    initial commit

---
+++ src/TelegramLoggerServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Logger;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class TelegramLoggerServiceProvider
+ * @package Logger
+ */
+class TelegramLoggerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/telegram-logger.php', 'telegram-logger');
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/views', 'laravel-telegram-logging');
+        $this->publishes([__DIR__.'/views' => base_path('resources/views/vendor/laravel-telegram-logging')], 'views');
+        $this->publishes([__DIR__ . '/../config/telegram-logger.php' => config_path('telegram-logger.php')], 'config');
+    }
+}
